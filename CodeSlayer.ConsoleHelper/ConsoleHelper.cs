3698f412b2da5acf52d01d1fcf72a27147532b96
Nathan Barnard <26314491+NathanBarnard@users.noreply.github.com>
2019-02-03 01:06:16 +0000

    Add initial source

---
+++ CodeSlayer.ConsoleHelper/ConsoleHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CodeSlayer.ConsoleHelper
+{
+    public static class ConsoleHelper
+    {
+        public static void AllocConsole()
+        {
+            Show();
+            var writer = new TextWriterTraceListener(Console.Out);
+            Debug.Listeners.Add(writer);
+        }
+
+        public static void Show()
+        {
+            // If we already have a console window, show it.
+            var handle = Native.GetConsoleWindow();
+            if (handle != IntPtr.Zero)
+            {
+                Native.ShowWindow(handle, Native.SW_SHOW);
+                return;
+            }
+
+            // Create a new console if we don't have one to attach to.
+            if (!Native.AttachConsole(-1))
+            {
+                Native.AllocConsole();
+            }
+
+
+            /*
+            Native.AllocConsole();
+            var stdHandle = Native.GetStdHandle(Native.STD_OUTPUT_HANDLE);
+            var safeFileHandle = new SafeFileHandle(stdHandle, true);
+            var fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+            var encoding = Encoding.GetEncoding(Native.MY_CODE_PAGE);
+            var standardOutput = new StreamWriter(fileStream, encoding);
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
+            */
+
+
+
+        }
+        public static void Hide()
+        {
+            var handle = Native.GetConsoleWindow();
+            if (handle != IntPtr.Zero)
+            {
+                Native.ShowWindow(handle, Native.SW_HIDE);
+            }
+        }
+    }
+}
---
+++ CodeSlayer.ConsoleHelper/Native.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CodeSlayer.ConsoleHelper
+{
+    internal static class Native
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool AttachConsole(int pid);
+
+        [DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public const int SW_HIDE = 0;
+        public const int SW_SHOW = 5;
+    }
+}
